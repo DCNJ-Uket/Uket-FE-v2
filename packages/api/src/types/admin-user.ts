af9c54fd6ecbefe9a90338daeb92875f17fd2a94
coggiee <zentechie7@gmail.com>
2025-03-24 14:51:55 +0900

    test: 어드민 목록 더미 데이터 생성 및 테스트 함수 추가

---
+++ packages/api/src/types/admin-user.ts
@@ -0,0 +1,26 @@
+export type Content = {
+  id: number;
+  name: string;
+  email: string;
+  organization: string;
+  authority: string;
+};
+
+export interface PaginationMeta {
+  pageNumber: number;
+  pageSize: number;
+  totalElements: number;
+  totalPages: number;
+  first: boolean;
+  last: boolean;
+  empty: boolean;
+}
+
+export interface AdminUserListResponse extends PaginationMeta {
+  content: Content[];
+}
+
+export interface AdminUseDefaultResponse {
+  adminId: Content["id"];
+  name: Content["name"];
+}
