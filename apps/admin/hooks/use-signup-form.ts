47b95f15a21545e1f7887f2d08b4732350b9ce14
SonSuBin129 <ttokky0129@gmail.com>
2025-03-24 10:56:03 +0900

    fix: 불필요한 message 제거

--- apps/admin/hooks/use-signup-form.ts
+++ apps/admin/hooks/use-signup-form.ts
@@ -13,10 +13,7 @@ const passwordRegex =
 
 const SignupFormSchema = z
   .object({
-    email: z
-      .string()
-      .email({ message: "유효하지 않은 이메일입니다." })
-      .min(1, { message: "이메일을 입력해주세요." }),
+    email: z.string().email(),
     password: z
       .string()
       .min(8, {
