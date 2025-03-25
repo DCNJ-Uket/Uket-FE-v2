41b59efc164554f38156e3a0943c1eaed8b5a726
coggiee <zentechie7@gmail.com>
2025-03-25 12:02:39 +0900

    refactor: 토스트 핸들러 로직 수정

--- packages/api/src/error/handler.ts
+++ packages/api/src/error/handler.ts
@@ -1,15 +1,23 @@
-import { routerTo } from "@uket/util/global-router";
-
 import { toast } from "@uket/ui/components/ui/sonner";
 import CustomAxiosError from "./default";
 
 /** 성공 시 토스트 */
-export const SUCCESS_TOAST = {
+export const TOAST_HANDLER: Record<
+  string,
+  {
+    onSuccess: () => void;
+    onError?: (error: CustomAxiosError) => void;
+  }
+> = {
   deleteUser: {
     onSuccess: () => {
-      // TODO: 라우터 처리 - 현재 페이지 이동 후 토스트가 사라짐
-      routerTo("/");
       toast.success("정상적으로 탈퇴되었습니다.");
+    },
+    onError: error => {
+      toast.error(`${error.errorContent?.title || "에러가 발생했어요"}`, {
+        description:
+          error.errorContent?.description || "잠시 후 다시 시도해 주세요.",
+      });
     },
   },
   cancelTicket: {
@@ -22,13 +30,16 @@ export const SUCCESS_TOAST = {
       toast.success("초대 이메일을 전송했습니다.");
     },
   },
+  deleteAdmin: {
+    onSuccess: () => {
+      toast.success("선택하신 멤버를 삭제했습니다.");
+    },
+  },
 };
 
 /** 실패 시 토스트 */
 export const onErrorHandler = (error: CustomAxiosError) => {
-  if (error.isToast) {
-    toast.error(`${error.errorContent?.title || "에러가 발생했어요"}`, {
-      description: error.errorContent?.description || "잠시 후 시도해 주세요.",
-    });
-  }
+  toast.error(`${error.errorContent?.title || "에러가 발생했어요"}`, {
+    description: error.errorContent?.description || "잠시 후 시도해 주세요.",
+  });
 };
