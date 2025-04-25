ca8595838952c8327e54c935416d494945947a72
SonSuBin129 <ttokky0129@gmail.com>
2025-04-25 21:46:43 +0900

    feat: 예매내역관리 행사 필터 추가

--- apps/admin/hooks/use-ticket-book-params.ts
+++ apps/admin/hooks/use-ticket-book-params.ts
@@ -7,6 +7,7 @@ export interface SearchParams {
   page?: number;
   searchType?: SearchType | null;
   searchValue?: string | null;
+  uketEventId?: number;
 }
 
 export const useTicketBookParams = () => {
@@ -16,6 +17,7 @@ export const useTicketBookParams = () => {
   const page = Number(searchParams.get("page")) || 1;
   const searchType = searchParams.get("searchType") || DEFAULT_SEARCH_TYPE;
   const searchValue = searchParams.get("searchValue") || "";
+  const uketEventId = Number(searchParams.get("uketEventId")) || undefined;
 
   const updateQuery = (params: SearchParams) => {
     const newParams = new URLSearchParams(searchParams.toString());
@@ -32,6 +34,7 @@ export const useTicketBookParams = () => {
     page,
     searchType,
     searchValue,
+    uketEventId,
     updateQuery,
   };
 };
