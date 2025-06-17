dbc9b577d9e9d54fde53816f15ff938263608768
SonSuBin129 <ttokky0129@gmail.com>
2025-06-17 23:43:52 +0900

    feat: 티켓 정보 불러오기 api 연결

--- apps/web/app/(auth)/buy-ticket/_components/select/datetime-select-field.tsx
+++ apps/web/app/(auth)/buy-ticket/_components/select/datetime-select-field.tsx
@@ -4,8 +4,8 @@ import { useEffect } from "react";
 import DateTimeButton from "./datetime-button";
 
 interface DateTimeSelectFieldProps {
-  dates: string[];
-  times: { date: string; remaining: number }[];
+  dates: { id: number; date: string }[];
+  times: { id: number; time: string; remaining: number }[];
   selectedDate: string;
   selectedTime: string | null;
   setSelectedDate: (date: string) => void;
@@ -20,11 +20,11 @@ export default function DateTimeSelectField({
   setSelectedDate,
   setSelectedTime,
 }: DateTimeSelectFieldProps) {
-  const selected = times.find(t => t.date === selectedTime);
+  const selected = times.find(t => t.time === selectedTime);
   const showWarning =
     selected &&
     formatDate(selectedDate, "compact") ===
-      formatDate(selected.date, "compact") &&
+      formatDate(selected.time, "compact") &&
     selected.remaining <= 10;
 
   const isOnlyOneDate = dates.length === 1;
@@ -32,7 +32,7 @@ export default function DateTimeSelectField({
 
   useEffect(() => {
     if (isOnlyOneTime) {
-      setSelectedTime(times[0]!.date);
+      setSelectedTime(times[0]!.time);
     }
   }, [isOnlyOneTime, selectedTime, times, setSelectedTime]);
 
@@ -41,8 +41,8 @@ export default function DateTimeSelectField({
       <div className="flex flex-col gap-3">
         <h3 className="font-medium">날짜 선택</h3>
         <div className="flex gap-2 flex-wrap">
-          {dates.map(date => (
-            <div key={date} onClick={() => setSelectedDate(date)}>
+          {dates.map(({ id, date }) => (
+            <div key={id} onClick={() => setSelectedDate(date)}>
               <DateTimeButton
                 isDate
                 date={date}
@@ -58,17 +58,17 @@ export default function DateTimeSelectField({
         <div className="flex flex-col gap-3">
           <h3 className="font-medium">시간 선택</h3>
           <div className="flex gap-2 flex-wrap">
-            {times.map(({ date, remaining }) => {
+            {times.map(({ id, time, remaining }) => {
               const isDisabled = remaining === 0;
               return (
                 <div
-                  key={date}
-                  onClick={() => !isDisabled && setSelectedTime(date)}
+                  key={id}
+                  onClick={() => !isDisabled && setSelectedTime(time)}
                 >
                   <DateTimeButton
                     isDate={false}
-                    date={date}
-                    selected={selectedTime === date}
+                    date={time}
+                    selected={selectedTime === time}
                     disabled={isDisabled}
                     isOnlyOne={isOnlyOneTime}
                   />
