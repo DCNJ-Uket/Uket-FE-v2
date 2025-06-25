017c95dc3247daedb04b00158ef8d255f8c7f01a
coggiee <zentechie7@gmail.com>
2025-06-25 17:48:42 +0900

    chore: 주석 삭제

--- apps/web/app/(auth)/my-info/_components/general-info.tsx
+++ apps/web/app/(auth)/my-info/_components/general-info.tsx
@@ -135,7 +135,6 @@ export default function GeneralInfo({ userInfo }: GeneralInfoProps) {
             }
             error={errors.phoneNumber}
           />
-          {/* <InfoItem title="사용자구분" content={userInfo.universityName} /> */}
         </div>
       </main>
     </main>
