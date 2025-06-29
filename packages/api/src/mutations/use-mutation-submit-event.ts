8ca5d2888d2f5c1b2d8f0274546212db5c46eb0c
zae._.hyeong <58213558+zae-hyeong@users.noreply.github.com>
2025-06-29 21:29:09 +0900

    feat, fix/UKET-184: 입장 그룹 오류 수정 (#71)

--- packages/api/src/mutations/use-mutation-submit-event.ts
+++ packages/api/src/mutations/use-mutation-submit-event.ts
@@ -1,5 +1,5 @@
 import { useMutation } from "@tanstack/react-query";
-import { format } from "@uket/util/time";
+import { format, formatTime } from "@uket/util/time";
 import { fetcherAdmin } from "../admin-instance";
 import { getQueryClient } from "../get-query-client";
 import { adminEventInfo } from "../queries/admin-event-info";
@@ -121,12 +121,23 @@ export const useMutationSubmitEvent = (
         ticketingEndDateTime:
           params.ticketingDate.ticketingEndDateTime.toISOString(),
       };
-      const entryGroup = params.entryGroup.map((entry) => {
-        return {
-          ticketCount: entry.ticketCount,
-          entryStartTime: `${entry.entryStartTime.hour}:${entry.entryStartTime.minute}:00`,
-        };
-      });
+      const entryGroup =
+        params.entryGroup.length === 0
+          ? params.eventRound.map(round => {
+              return {
+                ticketCount: params.totalTicketCount,
+                entryStartTime: round.startTime,
+              };
+            })
+          : params.entryGroup.map(entry => {
+              return {
+                ticketCount: entry.ticketCount,
+                entryStartTime: formatTime(
+                  entry.entryStartTime.hour,
+                  entry.entryStartTime.minute,
+                ),
+              };
+            });
       const imageIds = {
         uketEventImageId: params.uketEventImageId.id,
         thumbnailImageId: params.thumbnailImageId.id,
@@ -146,7 +157,9 @@ export const useMutationSubmitEvent = (
         depositUrl: params.paymentInfo.depositUrl,
       };
 
-      const noLimit = (buyTicketLimit <= 0 ? "제한 없음" : "제한") as SubmitEventRequestParams["noLimit"];
+      const noLimit = (
+        buyTicketLimit <= 0 ? "제한 없음" : "제한"
+      ) as SubmitEventRequestParams["noLimit"];
 
       const formattedData = {
         eventName,
