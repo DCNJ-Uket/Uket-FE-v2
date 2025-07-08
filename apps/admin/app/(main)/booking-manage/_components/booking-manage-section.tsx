d9777b8c2a95aed110a535c3bdf583c695840b99
coggiee <zentechie7@gmail.com>
2025-07-08 12:53:40 +0900

    fix: BookingManageSection에서 '지인'을 '공연자'로 변경 및 관련 타입 수정

--- apps/admin/app/(main)/booking-manage/_components/booking-manage-section.tsx
+++ apps/admin/app/(main)/booking-manage/_components/booking-manage-section.tsx
@@ -85,10 +85,10 @@ export const columns = (
     },
   },
   {
-    accessorKey: "friend",
+    accessorKey: "performer",
     header: "지인",
     cell: ({ row }) => {
-      return <div>{row.original.friend}</div>;
+      return <div>{row.original.performer}</div>;
     },
   },
 ];
@@ -105,6 +105,7 @@ export default function BookingManageSection() {
     page,
     uketEventId,
   });
+  
 
   const handleTicketSearch = (type: SearchType, value: string) => {
     if (value.trim().length > 0) {
@@ -126,7 +127,7 @@ export default function BookingManageSection() {
     { key: "updatedDate", label: "업데이트 일시" },
     { key: "orderDate", label: "주문 일시" },
     { key: "ticketStatus", label: "티켓 상태" },
-    { key: "friend", label: "지인" },
+    { key: "performer", label: "지인" },
   ];
 
   return (
